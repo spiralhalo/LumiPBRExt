bdf194813b2b8880437723f362f677aaac1189b5
spiralhalo <re.nanashi95@gmail.com>
2020-12-31 20:17:24 +0700

    Limit height difference between neighboring pixels

--- assets/lumi/shaders/lib/bump2.glsl
+++ assets/lumi/shaders/lib/bump2.glsl
@@ -8,6 +8,7 @@
 #ifndef VERTEX_SHADER
 #ifdef LUMI_BUMP
 #define _bump_height2(x) (x.r + x.g + x.b) * 0.33333 * 2.0
+#define max_delta 0.2
 vec3 bump_normal2(sampler2D tex, vec3 normal, vec2 uvn, vec2 uvt, vec2 uvb, bool reverse)
 {
     vec3 tangentMove = l2_tangent * (reverse ? -1.0 : 1.0);
@@ -17,11 +18,16 @@ vec3 bump_normal2(sampler2D tex, vec3 normal, vec2 uvn, vec2 uvt, vec2 uvb, bool
     if (uvn.y < bump_topRightUv.y) { uvb = uvn; }
 
     vec3 texel = texture2D(tex, uvn, _cv_getFlag(_CV_FLAG_UNMIPPED) * -4.0).rgb;
-    vec3 origin = _bump_height2(texel) * normal;
+    float hn = _bump_height2(texel);
+    vec3 origin = hn * normal;
     texel = texture2D(tex, uvt, _cv_getFlag(_CV_FLAG_UNMIPPED) * -4.0).rgb;
-    vec3 tangent = tangentMove + _bump_height2(texel) * normal - origin;
+    float ht = _bump_height2(texel);
+    float delta = ht - hn;
+    vec3 tangent = tangentMove + min(hn + delta, hn + max_delta) * normal - origin;
     texel = texture2D(tex, uvb, _cv_getFlag(_CV_FLAG_UNMIPPED) * -4.0).rgb;
-    vec3 bitangent = bitangentMove + _bump_height2(texel) * normal - origin;
+    float hb = _bump_height2(texel);
+    delta = hb - hn;
+    vec3 bitangent = bitangentMove + min(hn + delta, hn + max_delta) * normal - origin;
 
     return normalize(cross(tangent, bitangent));
 }
